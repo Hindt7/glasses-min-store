3af047048bb78a8c25691e5e4d4ad7a9a2ed7e35
LinhTr <htra0007@student.monash.edu>
2025-04-14 00:47:17 +1000

    implement home page and logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,31 @@
 import { Link, Outlet } from "react-router-dom";
-import { BagProvider } from "./appContext";
-//import { useState } from "react";
+import { BagProvider } from "./appContext.jsx";
+import logo from "./assets/logo_md.png";
 
 export default function App() {
   //const [bag, setBag] = useState({});
 
   return (
     <>
-      <div className="flex flex-col h-screen min-w-100">
+      <div className="flex flex-col h-screen min-w-100 m-3">
         <div
           id="header"
-          className="w-full min-h-[80px] px-40 flex flex-row justify-between"
+          className="w-full min-h-[80px] md:px-30 flex flex-row justify-between md:pt-6"
         >
-          <div className="self-center">Logo</div>
+          <Link to={"/"}>
+          <img src={logo} alt="Logo" className="h-16 cursor-pointer"/>
+
+          </Link>
           <div className="flex justify-around">
             <Link
               to={"allproducts/"}
-              className="mr-5 pr-10 self-center cursor-pointer"
+              className="mr-5 pr-10 self-center cursor-pointer md:text-xl"
             >
               Our Collection
             </Link>
             <div className="self-center">
               <Link to={"shoppingbag/"}>
-                <button className="btn-primary">Bag</button>
+                <button className="btn-primary md:text-xl md:py-3">Your Bag</button>
               </Link>
             </div>
           </div>
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,7 +1,26 @@
+import firstShade from '../assets/first-shade.png';
+
 export default function Home() {
-    return (
-        <span className="self-center">
-            Home Page
-        </span>
-    );
+  return (
+    <>
+      <div id="wrapper" class="flex pt-20 gap-14 lg:px-20">
+        <div id="textbox-wrapper" class="w-3/5">
+          <p class="pt-6 italic font-special text-2xl/10 font-light">
+            Behind every great pair of sunglasses is a great nap waiting to
+            happen.
+          </p>
+          <p class="mt-10 font-black text-5xl/16 ">
+            Specialized in stylish cover —and power naps..zzZ
+          </p>
+          <button class="mt-14 btn-primary md:text-xl md:py-5 md:px-8">
+            Explore our collection
+          </button>
+        </div>
+
+            <img 
+          class="w-2/5 border-2 border-blue-900 h-100 object-contain bg-white" src={firstShade} alt="Black sunglasses"/>
+
+      </div>
+    </>
+  );
 }
